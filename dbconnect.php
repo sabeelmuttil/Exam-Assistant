4e8a46b85be54ce734540001e3c68b08c996a610
sabeel muttil <sabeelmuttil@gmail.com>
2017-01-10 18:04:38 +0000

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -7,7 +7,7 @@
 	define('DBHOST', 'localhost');
 	define('DBUSER', 'root');
 	define('DBPASS', '');
-	define('DBNAME', 'exam');
+	define('DBNAME', 'bookexam');
 	
 	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
 	$dbcon = mysql_select_db(DBNAME);
