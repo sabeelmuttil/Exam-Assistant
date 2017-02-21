a790129a38b276e08f439bfbb8000871d7f3580a
sabeel muttil <sabeelmuttil@gmail.com>
2017-02-21 16:38:16 +0530

    new db pls import it
    new db pls import it

--- db backup/bookexam.sql
+++ db backup/bookexam.sql
@@ -1,8 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 21, 2017 at 05:37 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `bookexam`
 --
-CREATE DATABASE IF NOT EXISTS `bookexam` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `bookexam`;
 
 -- --------------------------------------------------------
 
@@ -10,26 +26,33 @@ USE `bookexam`;
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `admid` int(100) NOT NULL,
+  `schlname` varchar(200) NOT NULL,
   `admname` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `admemail` varchar(100) NOT NULL,
-  `admpass` varchar(100) NOT NULL
+  `admpass` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `zip` int(100) NOT NULL,
+  `about` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Truncate table before insert `admin`
+-- Dumping data for table `admin`
 --
 
-TRUNCATE TABLE `admin`;
+INSERT INTO `admin` (`admid`, `schlname`, `admname`, `name`, `admemail`, `admpass`, `address`, `city`, `country`, `zip`, `about`) VALUES
+(1, 'wmo college muttil', 'sabeelmuttil', 'sabeel km', 'sabeelmuttil@gmail.com', '1f67913d9ee5dbc9986e9684a373fcdf1f65a73ddaa02a88960e8f88d809843c', 'kmk', 'muttil', 'India', 673122, 'im &quot;sabeelmuttil&quot; pls search google then see me');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -38,11 +61,6 @@ CREATE TABLE `users` (
   `year` varchar(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Truncate table before insert `users`
---
-
-TRUNCATE TABLE `users`;
 --
 -- Indexes for dumped tables
 --
@@ -59,6 +77,20 @@ ALTER TABLE `admin`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
