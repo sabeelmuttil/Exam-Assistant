b848dae180be810edaf64647718d32eae93280fe
muhammadanaswayanad <muhammadanaswayanad@gmail.com>
2017-03-11 11:18:50 +0530

    Issue: randomization issue same output on every instance
    Arguments:
    1. f1 starting position
    2. fn question endi or count
    
    Output
    
    output is written to output.txt

--- temp/seeder.cpp
+++ temp/seeder.cpp
@@ -15,20 +15,72 @@ inline bool exist(const std::string& name)
 
 int main (int argc, char *argv[])
 {
+								int f1=0;
+								int fn=0;
 								int COUNT=0;
-								//cout << argv[1] << endl;
+								cout << argv[1] << endl;
+								cout << argv[2] << endl;
 								//contains argument 1
 								//check if exam exists;
-								if(argc>1) {
-																COUNT=atoi(argv[1]);
+								if(argc>2) {
+																f1=atoi(argv[1]);
+																fn=atoi(argv[2]);
+																COUNT=fn-f1;
 								}
-								int seedval;
-								int trees[1000]={0};
+								//int trees[1000]={0};
 								string Stree[1000];
+								std::cout << "no string isssue" << '\n';
 								if (exist("exam.flag")) {
-																ifstream seedvalpool;
-																seedvalpool.open("seedval.pool");
-																seedvalpool>>seedval;
+																std::cout << "countinuation of exam" << '\n';
+																ifstream trees;//question list (selected)
+																trees.open("trees.pool");
+																std::cout << "opening question list..." << '\n';
+
+																for(int i = 0; i < COUNT; ++i)
+																{
+																								trees >> Stree[i];
+
+																}
+																//Shuffling algorithm
+																std::cout << "Shuffling ..." << '\n';
+
+																int rndnum=-99;
+																for (int i = 0; i < COUNT-2; i++) {
+																								do {
+																																rndnum=rand()%COUNT;
+																																std::cout << rndnum << '\n';
+																								} while(!(i<=rndnum&&COUNT>rndnum));
+																								//swap
+																								string ti;
+																								ti=Stree[i];
+																								Stree[i]=Stree[rndnum];
+																								Stree[rndnum]=ti;
+
+																}//End of algorithm
+																std::cout << "finished shuffling :)" << '\n';
+																trees.close();
+																ofstream output;
+																output.open("output.txt");
+																for (int i = 0; i < COUNT; i++) {
+																								output<<Stree[i]<<" ";
+																}
+
+
+								}
+								else{
+																//Starting new exam
+																//create file exam.flag
+																ofstream flag;
+																std::cout << "creating flag , starting new exam" << '\n';
+																flag.open("exam.flag");
+																ofstream wtrees;
+																wtrees.open("trees.pool");
+																//opened trees to write questions
+																for (int i = f1; i < fn; i++) {
+																								wtrees<<i<<" ";
+																}
+																//written leaves
+																wtrees.close();
 																ifstream trees;//question list (selected)
 																trees.open("trees.pool");
 																for(int i = 0; i < COUNT; ++i)
@@ -42,18 +94,22 @@ int main (int argc, char *argv[])
 																for (int i = 0; i < COUNT-2; i++) {
 																								do {
 																																rndnum=rand()%COUNT;
-																								} while(i<=rndnum&&COUNT>rndnum);
+																								} while(!(i<=rndnum&&COUNT>rndnum));
 																								//swap
 																								string ti;
 																								ti=Stree[i];
 																								Stree[i]=Stree[rndnum];
 																								Stree[rndnum]=ti;
 
+																}//End of algorithm
+																trees.close();
+																ofstream output;
+																output.open("output.txt");
+																for (int i = 0; i < COUNT; i++) {
+																								output<<Stree[i]<<" ";;
 																}
 
-								}
-								else{
-																//Starting exam
+
 
 								}
 								return 0;
